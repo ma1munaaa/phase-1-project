931ac311a4cbacafac30c435bbb20a27e8a9a663
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-07-10 09:22:40 +0300

    finished

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# phase-1-project
+# Bookstore Web Application
+
+This is a web application that showcases a collection of books available for purchase. The application is built using HTML, CSS, and JavaScript.
+
+## LIVE LINK 
+
+- 
+
+- 
+
+## Technologies Used
+
+The following technologies were used to develop this application:
+
+- HTML
+- CSS
+- JavaScript
+
+## Features
+
+- View the list of available books.
+- See the title, price, and image of each book.
+- Check the number of copies available for each book.
+
+## How to Use
+
+1. Clone the repository to your local machine.
+
+```bash
+
+- git clone 
+
+```
+
+2. Open the project directory.
+
+- cd phase-1-project
+
+
+3. Open the `index.html` file in your web browser.
+4. You will see a list of books with their details.
+5. Scroll through the list to view all the available books.
+
+# Book Data
+
+Each book object in the `books` array has the following properties:
+
+- `id` (number): The unique identifier of the book.
+- `title` (string): The title of the book.
+- `price` (string): The price of the book.
+- `image` (string): The URL of the book's image.
+- `copies` (number): The number of copies available for purchase.
+
+## Author
+
+This web application was developed by *Maimuna Mohamud*.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use and modify the code as per your needs.
+
+
+
---
+++ index.js
@@ -0,0 +1,67 @@
+document.getElementById('get-started-btn').addEventListener('click', () => {
+    // Fetch book data from JSON file
+    fetch('https://api.npoint.io/b7761f8d4b23c462d460')
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('welcome-container').style.display = 'none';
+        document.getElementById('content-container').style.display = 'block';
+  
+        const bookListContainer = document.getElementById('book-list');
+  
+        data.books.forEach(book => {
+          const bookElement = createBookElement(book);
+          bookListContainer.appendChild(bookElement);
+        });
+      });
+  });
+  
+  function createBookElement(book) {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+  
+    const bookInfoContainer = document.createElement('div');
+    bookInfoContainer.classList.add('book-info-container');
+    bookElement.appendChild(bookInfoContainer);
+  
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = book.title;
+    bookInfoContainer.appendChild(titleElement);
+  
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = book.description;
+    bookInfoContainer.appendChild(descriptionElement);
+  
+    const priceElement = document.createElement('p');
+    priceElement.textContent = `Price: ${book.price}`;
+    bookInfoContainer.appendChild(priceElement);
+  
+    const copiesElement = document.createElement('p');
+    copiesElement.textContent = `Copies: ${book.copies}`;
+    bookInfoContainer.appendChild(copiesElement);
+  
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+    bookInfoContainer.appendChild(buttonContainer);
+  
+    const buyButton = document.createElement('button');
+    buyButton.textContent = 'Buy';
+    buyButton.classList.add('buy-button');
+    buttonContainer.appendChild(buyButton);
+    buyButton.addEventListener('click', () => {
+      if (book.copies > 0) {
+        book.copies--;
+        copiesElement.textContent = `Copies: ${book.copies}`;
+      }
+    });
+  
+    const returnButton = document.createElement('button');
+    returnButton.textContent = 'Return';
+    returnButton.classList.add('return-button');
+    buttonContainer.appendChild(returnButton);
+    returnButton.addEventListener('click', () => {
+      book.copies++;
+      copiesElement.textContent = `Copies: ${book.copies}`;
+    });
+  
+    return bookElement;
+  }
